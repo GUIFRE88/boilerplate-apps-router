7e2fc386148ac50effbb465daf4df63aab447aef
GUIFRE88 <guilherme.fre073@gmail.com>
2025-03-17 07:33:52 -0300

    Add tests

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,20 @@ export default {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/app/**',
+    '!src/lib/registry.tsx',
+    '!src/styles/**',
+    '!src/types/**'
+  ],
   setupFilesAfterEnv: ['./.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js'
   }
 }
